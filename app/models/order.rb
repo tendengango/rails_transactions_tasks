916d6a72f4289ae6f8b54c1b8042563003d998d5
tendengango <framboutel@gmail.com>
2023-04-25 16:31:18 +0100

    rails_transaction_task

---
+++ app/models/order.rb
@@ -0,0 +1,14 @@
+class Order < ApplicationRecord
+  belongs_to :user
+  has_many :ordered_lists
+  has_many :items, through: :ordered_lists
+  accepts_nested_attributes_for :ordered_lists
+
+  def update_total_quantity
+    self.ordered_lists.each do |line_item|
+      item = Item.find_by(id: line_item.item_id)
+      item.total_quantity += line_item.quantity
+      item.save!
+    end
+  end
+end
